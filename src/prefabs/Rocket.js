087785e71cc1c0077b36db2a0f2d1cbfe0bc3249
Marlene Lopez <mlope109@ucsc.edu>
2022-04-20 15:32:14 -0700

    pointer movement modification

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -11,27 +11,27 @@ class Rocket extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(!this.isFiring) {
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize + this.width){
-                this.x += this.moveSpeed;
+        mouse.on
+    // pointer movement
+        "pointermove", (pointer) => {
+            if(!this.isFiring)
+            {this.x = Phaser.Math.Clamp(pointer.x, 47, 578);}
+            };
+        
+    // firing    
+        if(mouse.activePointer.leftButtonDown() && !this.isFiring) {
+            { 
+                this. isFiring = true;
+                this.sfxRocket.play();
             }
-        }
-
-        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
-            this.sfxRocket.play();
-            this.isFiring = true;
-        }
-
-        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
-            this.y -= this.moveSpeed;
-        }
-        if(this.y <= borderUISize * 3 + borderPadding){
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
-        }
+        if(this.isFiring && this.y >= 108) this.y -=2;  // move up
+        if(this.y <= 108);}                               // reset on miss}
+            {
+                this.isFiring = false;
+                this.y = 431;
+            }
     }
+    
     reset() {
         this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
