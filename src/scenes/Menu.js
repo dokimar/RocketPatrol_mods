038a42e2257dbdcdfd893b3d9e9da744c92c5fe9
Marlene Lopez <mlope109@ucsc.edu>
2022-04-20 16:47:28 -0700

    display highscore text on the menu screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -29,22 +29,30 @@ class Menu01 extends Phaser.Scene {
       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
       keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
-      // show menu text
+      // ----- show menu text -----
       menuConfig.fontSize = '46px'
       // set title
       this.add.text(game.config.width / 2, game.config.height / 2 - borderUIsize -
           borderPadding*4, 'BALLOON POP', menuConfig).setOrigin(0.5);
+      
       menuConfig.fontSize = '28px'
       // set instructions
       this.add.text(game.config.width / 2, game.config.height / 2, 'Use (S)(F) to move (left)(right) & (E) to fire',
           menuConfig).setOrigin(0.5);
+      
       menuConfig.color = '#000';
       // set easy vs hard modes
       this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize +
           borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
       // set 1 player vs 2 player game
-          this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize * 2 +
+      this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize * 2 +
           borderPadding * 2, 'Press ↓ for Two Player Game', menuConfig).setOrigin(0.5);
+      
+      // Display highscore text on the menu screen
+      this.add.text(borderUIsize + borderPadding * 17, borderUIsize + borderPadding * 2, 'HighScore: ',
+          menuConfig);
+      this.scoreRight = this.add.text(borderUIsize + borderPadding * 34, borderUIsize + borderPadding * 2, high_Score,
+          menuConfig);
     }
 
     update() {
@@ -124,6 +132,12 @@ class Menu01 extends Phaser.Scene {
       menuConfig.color = '#000';
       this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize +
           borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+       
+      // Display the highscore text on the menu screen
+      this.add.text(borderUIsize + borderPadding * 17, borderUIsize + borderPadding * 2, 'HighScore: ',
+          menuConfig);
+      this.scoreRight = this.add.text(borderUIsize + borderPadding * 34, borderUIsize + borderPadding * 2, high_Score,
+          menuConfig);
     }
 
     update() {
