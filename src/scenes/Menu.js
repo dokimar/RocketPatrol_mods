b52b5cb2dc9c021f31c1716e00b61aeec8dd8649
Marlene Lopez <mlope109@ucsc.edu>
2022-04-20 16:22:25 -0700

    update menu text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -32,6 +32,24 @@ class Menu extends Phaser.Scene {
       // define keys
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+      keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
+      // show menu text
+      menuConfig.fontSize = '46px'
+      // set title
+      this.add.text(game.config.width / 2, game.config.height / 2 - borderUIsize -
+          borderPadding*4, 'BALLOON POP', menuConfig).setOrigin(0.5);
+      menuConfig.fontSize = '28px'
+      // set instructions
+      this.add.text(game.config.width / 2, game.config.height / 2, 'Use (S)(F) to move (left)(right) & (E) to fire',
+          menuConfig).setOrigin(0.5);
+      menuConfig.color = '#000';
+      // set easy vs hard modes
+      this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize +
+          borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+      // set 1 player vs 2 player game
+          this.add.text(game.config.width / 2, game.config.height / 2 + borderUIsize * 2 +
+          borderPadding * 2, 'Press ↓ for Two Player Game', menuConfig).setOrigin(0.5);
     }
 
     update() {
