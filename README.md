4e3e2d912820c126d0f7e732de4360fd6178bf13
Marlene Lopez <mlope109@ucsc.edu>
2022-04-21 04:13:06 -0700

    commenting on code

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Implementing modifications to the Rocket Patrol tutorial game created in class
 UCSC CMPM120 Spring 2022
 04/20/2022
 
-Name: Marlene Lopez
+Author: Marlene Lopez
 Duration: 43 hours
 
+Assignment: Rocket Patrol Mods [~10–15 hours]
+Name of the Game: Roadkill
 Play the game:
 
 Points Breakdown:
@@ -14,5 +16,15 @@ Points Breakdown:
 (5)  Allow the player to control the rocket after it's fired
 (30) Implement a simultaneous two-player mode
 (60) Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+
+Cite Sources:
+For the sad looking pixel art, I made it myself using Photoshop.
+But for the three sound pieces, I got them off of opengameart.org
+    explosion.wav:
+        petri.nykanen (2017); https://opengameart.org/content/falling-body
+    blip_select.wav:
+        snabisch (2014); "Slowdown" https://opengameart.org/content/pack-8-bit-car-game-sound-effects
+    rocket_shot.wav:
+        snabisch (2014); "Starting the engine" https://opengameart.org/content/pack-8-bit-car-game-sound-effects
 
 Total Points: 105
--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
+// total points: 100
+// Marlene Lopez (mlope109@ucsc.edu)
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    // S(hrek) tier: 
+    // Implement a simultaneous two-player mode. 
+    // In this mode there are two rockets at the same time, 
+    // each with its own (key) controls, each capable of independent firing. 
+    // (30)
     scene: [Menu1, Menu2, SinglePlayer, TwoPlayer]
 }
 
 // reserve keyboard vars
+// Track a high score that persists across scenes and display it in the UI
+// (5)
+// Allow the player to control the Rocket after it's fired 
+// (5)
+
+(5)  Allow the player to control the rocket after it's fired
 let keyUP, keyDOWN, keyLEFT, keyRIGHT, keyE, keyS, keyF, keyR;
 let game = new Phaser.Game(config);
+
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic 
+// (to something other than sci-fi) 
+// (60)
+
+// I chose to make it roadkill themed and color coordinated the point system
+// display with the color of the car that represents the player
+// the goal is to run over as many racoons as possible and the art
+// I created for the game is meant to represent a road and cars crashing into
+// racoons that in the explosion become casualties as shown with blood
+// the sounds are also car sounds like starting the engine, screeching tires,
+// and a thud when the body of the animal collides with the car.
+// instead of sci-fi it is urban
 
 // set UI sizes
 let borderUIsize = game.config.height / 15;
