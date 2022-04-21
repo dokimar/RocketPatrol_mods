0a8479a54c53212c3b33050e84a299abba01280a
Marlene Lopez <mlope109@ucsc.edu>
2022-04-20 18:41:52 -0700

    change in spaceship movement

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -2,14 +2,15 @@
 class Spaceship extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame, pointValue){
         super(scene, x, y, texture, frame, pointValue);
+        // add object to existing scene
         scene.add.existing(this);
         this.points = pointValue;
-        this.moveSpeed = 2.5;
+        this.moveSpeed = game.settings.spaceshipSpeed;
     }
 
-    update(){
-        // move spaceship left
+    update() {
         this.x -= this.moveSpeed;
+
         if(this.x <= 0 - this.width){
             this.reset();
         }
